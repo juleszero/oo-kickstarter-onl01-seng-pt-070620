f16d3d497bf51ccfaf05c8fcdda96bae3911731e
Juliana Connors <juliana.g.connors@gmail.com>
2020-07-31 14:06:08 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,8 @@ class Project
   
   def add_backer(backer)
     @backers << backer 
-    backer.back_project(self) unless backer.backed_projects.include?(self)
+    #backer.back_project(self) unless backer.backed_projects.include?(self)
+    backer.backed_project << self
   end
   
 end
